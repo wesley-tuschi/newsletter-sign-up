c6ab2237f8d8f979e7bc561a2a44b44f8ffb0d27
Wesley Tuschi <levituschi@gmail.com>
2023-09-03 23:56:51 -0300

    feat: structure ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import iconList from './assets/images/icon-list.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stay updated!</h1>
+      <p><img src={iconList} alt="iconlist" /> Join 60,000+ product managers receiving monthly updates on:</p>
+      <p><img src={iconList} alt="iconlist" /> Product discovery and building what matters</p>
+  <p><img src={iconList} alt="iconlist" /> Measuring to ensure updates are a success</p>
+  <p><img src={iconList} alt="iconlist" /> And much more!</p>
+  <section>
+    <form>
+    <label for="email">Email adress</label>
+      <input type="email" placeholder="email@company.com" />
+      <button>Subscribe to monthly newsletter</button>
+      </form>
+  </section>
     </div>
   );
 }
